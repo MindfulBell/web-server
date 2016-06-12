92b1503ccf093bf393e6ea7da15879ed3473d3da
MindfulBell <tim.bell02@gmail.com>
2016-06-11 17:07:41 -0700

    changes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = 3000
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
-	res.send('ABOUT MOTHERFUCKER!')
+	res.send('ABOUT MOTHERFUCKER!!')
 })
 
 app.use(express.static(__dirname + '/public')) //neato, this lets us access ANYTHING within the folder: localhost:3000/index.html
