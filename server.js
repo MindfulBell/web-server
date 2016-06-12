48dab4af32b6dd3f17ecb75793b7a94f323ef471
MindfulBell <tim.bell02@gmail.com>
2016-06-11 20:33:32 -0700

    added P tag

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var middleware = require('./middleware');
-var favicon = require('serve-favicon');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -12,7 +11,6 @@ app.get('/about', middleware.requireAuthentication, function(req, res){
 })
 
 app.use(express.static(__dirname + '/public')) //neato, this lets us access ANYTHING within the folder: localhost:3000/index.html
-app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.listen(PORT, function(){
 	console.log('Server listening on port ' + PORT)
