5d92524b16729feab4cb11b84fbbb29affaf23c3
MindfulBell <tim.bell02@gmail.com>
2016-06-11 17:54:40 -0700

    added serve-favicon

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ let PORT = process.env.PORT || 3000;
 
 app.use(middleware.logger);
 
+
 app.get('/about', middleware.requireAuthentication, function(req, res){
 	res.send('ABOUT MOTHERFUCKER!!')
 })
 
 app.use(express.static(__dirname + '/public')) //neato, this lets us access ANYTHING within the folder: localhost:3000/index.html
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.listen(PORT, function(){
 	console.log(`Server listening on port ${PORT}!`)
